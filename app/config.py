24404804b5f9736196becbe03bae0dc3c549e06c
shehamane <shehamane@mail.ru>
2020-11-02 20:54:05 +0300

    switch to webooks

--- app/config.py
+++ app/config.py
@@ -1,2 +1,4 @@
 TOKEN = '1393659791:AAGA4278XfjZmmQk54Bh1dU70DJtaeD3RMg'
 URL = 'https://api.telegram.org/bot{}/'.format(TOKEN)
+
+# https://api.telegram.org/bot1393659791:AAGA4278XfjZmmQk54Bh1dU70DJtaeD3RMg/setWebhook?url=https://7ce3bb52cc30.ngrok.io/
--- app/main.py
+++ app/main.py
@@ -1,19 +1,16 @@
 from flask import Flask
+from flask import request
+from flask import jsonify
 from app.config import URL
 import requests
 import json
+
+app = Flask(__name__)
 
 
 def write_json(data, filename='../log/response.json'):
     with open(filename, 'w') as file:
         json.dump(data, file, indent=2, ensure_ascii=False)
-
-
-def get_updates():
-    url = URL + 'getUpdates'
-    response = requests.get(url)
-    # write_json(response.json())
-    return response.json()
 
 
 def send_message(chat_id, text='lolkek'):
@@ -23,21 +20,29 @@ def send_message(chat_id, text='lolkek'):
     return response.json()
 
 
-def echo_last_message(last_id):
-    update = get_updates()['result'][-1]
-    update_id = update['update_id']
-    if update_id != last_id:
-        chat_id = update['message']['chat']['id']
-        text = update['message']['text']
+def handle_update(u):
+    if 'message' in u:
+        chat_id = u['message']['chat']['id']
+        text = u['message']['text']
         send_message(chat_id, text)
-    return update_id
+
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    if request.method == 'POST':
+        update = request.get_json()
+        write_json(update)
+        handle_update(update)
+        return jsonify(update)
+    return '<h1>Hello</h1>'
 
 
 def main():
-    last_message_id = None
-    while True:
-        last_message_id = echo_last_message(last_message_id)
+    # last_message_id = None
+    # while True:
+    #     last_message_id = echo_last_message(last_message_id)
+    pass
 
 
 if __name__ == '__main__':
-    main()
+    app.run()
