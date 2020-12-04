51617fa960687efe2e719e61e8598232b90f899a
shehamane <shehamane@mail.ru>
2020-12-05 00:28:52 +0300

    throttling message translated

--- src/middlewares/throttling.py
+++ src/middlewares/throttling.py
@@ -41,7 +41,7 @@ class ThrottlingMiddleware(BaseMiddleware):
             key = f"{self.prefix}_message"
         delta = throttled.rate - throttled.delta
         if throttled.exceeded_count <= 2:
-            await message.reply('Too many requests! ')
+            await message.reply('Вы пишете слишком часто!')
         await asyncio.sleep(delta)
         thr = await dispatcher.check_key(key)
         if thr.exceeded_count == throttled.exceeded_count:
