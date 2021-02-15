8b53befe7c4ec98558f6aa2f100f8a1fd6e42fce
shehamane <shehamane@mail.ru>
2021-02-16 00:22:11 +0300

    product addition one-message

--- src/handlers/admin/products.py
+++ src/handlers/admin/products.py
@@ -12,23 +12,30 @@ from utils.misc.files import download_product_image
 
 
 @dp.callback_query_handler(text="new", state=CatalogEdit.ProductsWatching)
-async def add_new_product(call: CallbackQuery):
+async def add_new_product(call: CallbackQuery, state: FSMContext):
     await CatalogEdit.ProductInfoRequest.set()
-    await call.message.answer("Введите информацию о новом продукте по шаблону:\n"
-                              "НАЗВАНИЕ\n"
-                              "ОПИСАНИЕ\n"
-                              "ЦЕНА\n", reply_markup=cancel_kb)
+
+    text = "Введите информацию о новом продукте по шаблону:\n"
+    "НАЗВАНИЕ\n"
+    "ОПИСАНИЕ\n"
+    "ЦЕНА\n"
+
+    async with state.proxy() as state_data:
+        await state_data["catalog_message"].edit_caption(text, reply_markup=cancel_kb)
 
 
 @dp.message_handler(state=CatalogEdit.ProductInfoRequest)
-async def confirm_addition(message: Message, state: FSMContext):
+async def get_info(message: Message, state: FSMContext):
     strings = message.text.split('\n')
-    if len(strings) != 3:
-        await message.answer("Вы неверно ввели информацию. Сообщение должно содержать 3 строки, как указано в шаблоне")
-    else:
-        async with state.proxy() as state_data:
+    async with state.proxy() as state_data:
+        if len(strings) != 3:
+            await state_data["catalog_message"].edit_caption(
+                "Вы неверно ввели информацию. Сообщение должно содержать 3 строки,"
+                " как указано в шаблоне. Попробуйте снова")
+        else:
             product_id = await db.create_product(strings[0], strings[1], int(strings[2]), state_data["category_id"])
-            await message.answer("Продукт успешно создан!", reply_markup=download_image_kb)
+            await state_data["catalog_message"].edit_caption("Продукт успешно создан",
+                                                             reply_markup=download_image_kb)
 
             state_data["product_id"] = product_id
             await CatalogEdit.ProductImageWaiting.set()
@@ -38,17 +45,16 @@ async def confirm_addition(message: Message, state: FSMContext):
 async def download_image(call: CallbackQuery, state: FSMContext):
     await CatalogEdit.ProductImageRequest.set()
 
-    await call.message.answer("Отправьте изображение товара")
+    async with state.proxy() as state_data:
+        await state_data["catalog_message"].edit_caption("Отправьте изображение товара")
 
 
 @dp.callback_query_handler(text="without_image", state=CatalogEdit.ProductImageWaiting)
 async def change_state(call: CallbackQuery, state: FSMContext):
-    await call.message.delete_reply_markup()
     async with state.proxy() as state_data:
-        await call.message.answer_photo(InputFile(IMG_CATALOG_PATH), caption="Изображение добавлено!",
-                                        reply_markup=await get_admin_products_kb(
-                                            await db.get_products_by_page(state_data["category_id"], 0), 1,
-                                            state_data["page_total"]))
+        await call.message.edit_reply_markup(await get_admin_products_kb(
+            await db.get_products_by_page(state_data["category_id"], 0), 1,
+            state_data["page_total"]))
 
         await CatalogEdit.ProductsWatching.set()
 
@@ -57,9 +63,10 @@ async def change_state(call: CallbackQuery, state: FSMContext):
 async def get_product_image(message: Message, state: FSMContext):
     async with state.proxy() as state_data:
         await download_product_image(state_data["product_id"], message.photo[-1])
-        await message.answer_photo(InputFile(IMG_CATALOG_PATH), caption="Изображение добавлено!",
-                                   reply_markup=await get_admin_products_kb(
-                                       await db.get_products_by_page(state_data["category_id"], 0), 1,
-                                       state_data["page_total"]))
+        await state_data["catalog_message"].edit_caption(caption="Изображение добавлено!",
+                                                         reply_markup=await get_admin_products_kb(
+                                                             await db.get_products_by_page(state_data["category_id"],
+                                                                                           0), 1,
+                                                             state_data["page_total"]))
 
         await CatalogEdit.ProductsWatching.set()
