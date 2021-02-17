a242d4ff131935eed2838c68fbd571cab2f9d55d
shehamane <shehamane@mail.ru>
2021-02-17 19:47:44 +0300

    fix issue #1

--- src/data/media_config.py
+++ src/data/media_config.py
@@ -1,2 +1,3 @@
 IMG_CATALOG_PATH = '../img/catalog.jpg'
 IMG_CART_PATH = '../img/cart.jpg'
+IMG_DEFAULT_PATH = '../img/default.png'
--- src/handlers/users/catalog.py
+++ src/handlers/users/catalog.py
@@ -7,7 +7,7 @@ from utils.misc.files import get_product_image_path
 from keyboards.inline import get_subcategories_kb, get_product_watching_kb
 from states import CatalogListing
 
-from data.media_config import IMG_CATALOG_PATH
+from data.media_config import IMG_CATALOG_PATH, IMG_DEFAULT_PATH
 
 from loader import dp
 from utils.db_api.api import db_api as db
@@ -21,6 +21,8 @@ async def send_product_info(message: Message, product):
     img_path = await get_product_image_path(product.id)
     if img_path:
         await message.edit_media(InputMediaPhoto(InputFile(img_path)))
+    else:
+        await message.edit_media(InputMediaPhoto(InputFile(IMG_DEFAULT_PATH)))
 
     await message.edit_caption(text)
 
