620519f3cd7bb83cbc0fde724032974abca20ae2
shehamane <shehamane@mail.ru>
2021-04-10 02:17:47 +0300

    product changing fix

--- src/handlers/admin/product_info.py
+++ src/handlers/admin/product_info.py
@@ -10,7 +10,7 @@ from keyboards.inline.general import confirmation_kb, cancel_kb
 from loader import dp
 from states import CatalogEdit
 from utils.db_api.api import db_api as db
-from utils.misc.files import download_product_image
+from utils.misc.files import download_product_image, get_product_image_path
 
 
 @dp.callback_query_handler(IsNumericFilterCallback(), state=CatalogEdit.ProductsWatching)
@@ -22,7 +22,7 @@ async def show_product_info(call: CallbackQuery, state: FSMContext):
         product = await db.get_product(int(call.data))
 
         await send_product_info(state_data["main_message"], product)
-        await state_data["main_message"].edit_reply_markup(product_info_kb)
+        await call.message.edit_reply_markup(product_info_kb)
 
 
 @dp.callback_query_handler(text="back", state=CatalogEdit.ProductWatching)
@@ -59,7 +59,7 @@ async def new_info_request(call: CallbackQuery, state: FSMContext):
     await CatalogEdit.ProductInfoChangeRequest.set()
     async with state.proxy() as state_data:
         state_data["edit_type"] = str(call.data)
-        await state_data["main_message"].edit_caption("Введите новое значение", reply_markup=cancel_kb)
+        state_data["main_message"] = await call.message.edit_caption("Введите новое значение", reply_markup=cancel_kb)
 
 
 @dp.message_handler(state=CatalogEdit.ProductInfoChangeRequest)
@@ -69,14 +69,14 @@ async def change_info(message: Message, state: FSMContext):
     async with state.proxy() as state_data:
         if state_data["edit_type"] == "change_name":
             await db.change_product_name(state_data["product_id"], value)
-            await show_last_product_info(None, state)
+            await show_current_product(state_data["main_message"], state)
         elif state_data["edit_type"] == "change_description":
             await db.change_product_description(state_data["product_id"], value)
-            await show_last_product_info(None, state)
+            await show_current_product(state_data["main_message"], state)
         elif state_data["edit_type"] == "change_price":
             if value.isnumeric():
                 await db.change_product_price(state_data["product_id"], int(value))
-                await show_last_product_info(None, state)
+                await show_current_product(state_data["main_message"], state)
             else:
                 await state_data["main_message"].edit_caption("Введите новое значение корректно",
                                                               reply_markup=cancel_kb)
@@ -92,20 +92,34 @@ async def request_image(call: CallbackQuery):
 @dp.message_handler(content_types=['photo'], state=CatalogEdit.ProductImageChangeRequest)
 async def change_image(message: Message, state: FSMContext):
     async with state.proxy() as state_data:
-        await download_product_image(state_data["product_id"], message.photo[-1])
+        product = await db.get_product(state_data["product_id"])
 
-    await state.update_data({"is_photo": True})
-    await show_last_product_info(None, state)
+    await download_product_image(product.id, message.photo[-1])
+    img_path = await get_product_image_path(product.id)
+    text = f'{product.name}\n' \
+           f'{product.description}\n' \
+           f'Цена: {product.price} р.'
+
+    async with state.proxy() as state_data:
+        state_data["main_message"] = await message.answer_photo(InputFile(img_path), caption=text,
+                                                                reply_markup=product_info_kb)
+    await CatalogEdit.ProductWatching.set()
 
 
 @dp.callback_query_handler(text=["no", "cancel"],
                            state=[CatalogEdit.ProductDeletionConfirmation, CatalogEdit.ProductInfoChangeRequest,
                                   CatalogEdit.ProductImageChangeRequest])
-async def show_last_product_info(_, state: FSMContext):
+async def return_to_product(call: CallbackQuery, state: FSMContext):
+    await show_current_product(call.message, state)
+
+
+async def show_current_product(message: Message, state: FSMContext):
     await CatalogEdit.ProductWatching.set()
     async with state.proxy() as state_data:
         product = await db.get_product(state_data["product_id"])
 
-        state_data["main_message"] = await send_product_info(state_data["main_message"], product,
-                                                             state_data["is_photo"])
-        await state_data["main_message"].edit_reply_markup(product_info_kb)
+        text = f'{product.name}\n' \
+               f'{product.description}\n' \
+               f'Цена: {product.price} р.'
+
+        await message.edit_caption(text, reply_markup=product_info_kb)
